cacc44f1651453ccf6e9938374a04c33406b091e
xtreme <boreevyuri@gmail.com>
2019-01-02 17:35:12 +0300

    Add Express.js to boilerplate

---
+++ src/server/web.server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+class WebServer {
+  constructor() {
+    this.app = express()
+    this.app.use(express.static('dist/public'))
+  }
+
+  start() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.server = this.app.listen(3000, function () {
+          resolve()
+        })
+      } catch (e) {
+        console.error(e)
+        reject(e)
+      }
+    })
+  }
+
+  stop() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.server.close(() => {
+          resolve()
+        })
+      } catch (e) {
+        console.error(e)
+        reject(e)
+      }
+    })
+  }
+}
+
+export default WebServer
