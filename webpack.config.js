c1d2390f273dd9c82493fd29bdc86f9215d7e2fa
xtreme <boreevyuri@gmail.com>
2019-01-03 14:44:59 +0300

    ADD webpack, webpack config, new build rule

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,43 @@
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+
+const moduleObj = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }
+  ]
+}
+
 const client = {
   entry: {
     'client': './src/client/index.js'
-  }
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj
 }
 
 const server = {
   entry: {
     'server': './src/server/index.js'
-  }
+  },
+  target: 'node',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: moduleObj,
+  externals: [
+    nodeExternals()
+  ]
 }
 
 module.exports = [client, server]
