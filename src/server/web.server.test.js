1b5465fd5eb730792c47301e5a0ce4c0337c12cd
xtreme <boreevyuri@gmail.com>
2019-01-03 12:55:23 +0300

    ADD jest with hack for async ADD web.server.test.js

---
+++ src/server/web.server.test.js
@@ -0,0 +1,23 @@
+import WebServer from './web.server'
+import expect from 'expect'
+
+//Hack for jest with async
+import regeneratorRuntime from "regenerator-runtime";
+
+describe('Started', () => {
+  let webServer = null
+
+  beforeAll(() => {
+    webServer = new WebServer()
+  })
+
+  test('should start and trigger a callback', async () => {
+    let promise = webServer.start()
+    await expect(promise).resolves.toBeUndefined()
+  })
+
+  test('should stop and trigger a callback', async () => {
+    let promise = webServer.stop()
+    await expect(promise).resolves.toBeUndefined()
+  })
+})
