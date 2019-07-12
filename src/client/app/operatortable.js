a0ab86433bb064326194b502992582616326d4c3
xtreme <boreevyuri@gmail.com>
2019-07-12 18:05:09 +0300

    rewrite OperTable to function

--- src/client/app/operatortable.js
+++ src/client/app/operatortable.js
@@ -1,45 +1,36 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Title from './title'
 import Table from './table'
 
-const operatorColumns = [
+const columnNames = [
   'Operator',
   'Queue',
   'Last Call',
   'Status'
 ]
 
-class OperatorTable extends Component {
-
-
-  render() {
-
-    const operators = this.props.operators
-
-    return (
-      <div>
-        <Title
-          title={'Operators'}
-          subTitle={[
-            {
-              count: operators.length,
-              title: 'ops'
-            },
-            {
-              count: operators.filter(i => +i.status !== 2 && +i.paused !== 1).length,
-              title: 'reachable'
-            }
-          ]}
-        />
-
-        <Table
-          columnNames={operatorColumns}
-          mainStyleClass={'oper'}
-          children={operators}
-        />
-      </div>
-    )
-  }
-}
+const OperatorTable = ({operators}) => (
+  <div>
+    <Title
+      title={'Operators'}
+      subTitle={[
+        {
+          count: operators.length,
+          title: 'ops'
+        },
+        {
+          count: operators.filter(i => +i.status !== 2 && +i.paused !== 1).length,
+          title: 'reachable'
+        }
+      ]}
+    />
+    <Table
+      columnNames={columnNames}
+      mainStyleClass={'oper'}
+    >
+      {operators}
+    </Table>
+  </div>
+)
 
 export default OperatorTable
