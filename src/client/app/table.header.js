f64c1b3c704b36982e276584215742b0b8742d9c
xtreme <boreevyuri@gmail.com>
2019-07-12 18:11:52 +0300

    rewrite TableHeader to function

--- src/client/app/table.header.js
+++ src/client/app/table.header.js
@@ -1,22 +1,13 @@
 import React from 'react'
 
-class TableHeader extends React.Component {
-
-  render() {
-
-    const header = this.props.columnNames.map((i, index) => (
-        <div key={index} className={'cell'}>
-          <h4>{i}</h4>
-        </div>
-      )
-    )
-
-    return (
-      <div className={ this.props.mainStyleClass + ' header'}>
-        {header}
+const TableHeader = ({mainStyleClass, columnNames}) => (
+  <div className={`${mainStyleClass} header`}>
+    {columnNames.map((text, index) => (
+      <div key={index} className={'cell'}>
+        <h4>{text}</h4>
       </div>
-    )
-  }
-}
+    ))}
+  </div>
+)
 
 export default TableHeader
