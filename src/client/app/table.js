70623bcded94ca3fd6ff797bf401fbd575e3c531
xtreme <boreevyuri@gmail.com>
2019-07-12 18:36:25 +0300

    move TableHeader into Table

--- src/client/app/table.js
+++ src/client/app/table.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import TableHeader from './table.header'
+// import TableHeader from './table.header'
 import {useTranslation} from 'react-i18next'
 
 const Table = ({columnNames: columns, mainStyleClass: className, children}) => {
   const {t} = useTranslation()
   return (
     <div className={'table'}>
-      <TableHeader
-        columns={columns.map(el => t(el))}
-        className={className}
-      />
 
+      {/*Table header*/}
+      <div className={`${className} header`}>
+        {
+          columns.map((text, index) => (
+            <div key={index} className={'cell'}>
+              <h4>{t(text)}</h4>
+            </div>
+          ))
+        }
+      </div>
+      {/*Table content*/}
       {children.map((el, index) => (
         <div key={index} className={className}>
           {el.render()}
